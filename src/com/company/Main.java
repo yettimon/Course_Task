d28a0e8d6f0858e0b1098f74ce691396266e8d2e
Dmitriy Stavnichuk <globalobjectiveart@gmail.com>
2020-06-24 10:33:41 +0300

    Updating new files, cause light turned off

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
@@ -23,11 +25,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         // creating array of distinct words
-       ArrayList<String> DistinctWords = new ArrayList<>();
-       // write into file
-       FileWriter myWriter = new FileWriter("filename.txt");
-       String fileHeader = " -------HEADER FOR HARRY POTTER TASK--------------";
-       myWriter.write(fileHeader + "\n");
+        ArrayList<String> DistinctWords = new ArrayList<>();
+        // write into file
+        FileWriter myWriter = new FileWriter("filename.txt");
+        String fileHeader = " -------HEADER FOR HARRY POTTER TASK--------------";
+        myWriter.write(fileHeader + "\n");
 
         String text = new String(Files.readAllBytes(Paths.get(
                 "E:\\IdeaProjects\\Course_Task\\src\\com\\company\\Harry.txt")));
@@ -37,6 +39,7 @@ public class Main {
                 .replaceAll("[^a-zA-Z0-9' ]+", "");
 
         String[] distWords = clearedText.split(" ");
+
         // finding distinct words and their occurrences;
         for (int i = 0; i < distWords.length; i++) {
             int distCount = 1;
@@ -48,8 +51,8 @@ public class Main {
             }
             if (distCount > 1 && distWords[i] != "0" && i < 20) {
 
-               DistinctWords.add(distWords[i]);
-               DistinctWords.add(Integer.toString(distCount));
+                DistinctWords.add(distWords[i]);
+                DistinctWords.add(Integer.toString(distCount));
 
                 System.out.println("First 20 pairs of distinct words: "
                         + distWords[i] + " occurs "
@@ -58,8 +61,7 @@ public class Main {
                         + distWords[i] + " occurs "
                         + distCount + " times ");
                 myWriter.write(System.lineSeparator());
-            }
-            else{
+            } else {
                 break;
             }
         }
@@ -67,13 +69,12 @@ public class Main {
         //finding names from book
 
         // Task 2. Count the lines where the names "Harry" is met.
-        List<String> keys = new ArrayList<>();
         Map<String, Integer> twWords = new LinkedHashMap<>();
 
-        String[] names = {"Harry","Hermiona","Ron","Dambldoor", "Voldemort" };
+        String[] names = {"Harry", "Hermiona", "Ron", "Dambldoor", "Voldemort"};
         String words2[] = text.split(" ");
         int counter = 0;
-        for (int i = 0; i <names.length ; i++) {
+        for (int i = 0; i < names.length; i++) {
             for (int f = 0; f < words2.length; f++) {
                 if (names[i].equals(words2[f]))
                     counter++;
@@ -87,7 +88,6 @@ public class Main {
 
         System.out.println(DistinctWords);
         myWriter.write(String.valueOf(DistinctWords));
-        myWriter.close();
 
 
         List<String> IntoArray = new ArrayList<>();
@@ -100,16 +100,80 @@ public class Main {
         // distincs by Map Method
         Map<String, Integer> map = new TreeMap<String, Integer>();
 
-        for(String word: IntoArray) {
+        for (String word : IntoArray) {
             // if word is not in the map
-            if(!map.containsKey(word))
+            if (!map.containsKey(word))
                 map.put(word, 0);
             map.put(word, map.get(word) + 1);
         }
-        for(String word: map.keySet())
-            if (map.get(word) > 1)
-            System.out.println(word + " occurs " + map.get(word) + " times");
+        for (String word : map.keySet())
+            if (map.get(word) > 1) ;
+        //           System.out.println(word + " occurs " + map.get(word) + " times");
 
+        // calculate number of occurancies by using, task 2
+        Map<String, Integer> distinctMap = new HashMap<>();
+        for (int i = 0; i < distWords.length; i++) {
+            if (!distinctMap.containsKey(distWords[i])) {
+                distinctMap.put(distWords[i], 1);
+            } else {
+                Integer countNew = distinctMap.get(distWords[i]);
+                distinctMap.put(distWords[i], countNew + 1);
+            }
+
+        }
+        distinctMap.forEach((el, a) -> {
+            System.out.println(("Disting word " + el
+                    + "repeats: " + a + " times "));
+        });
+        // sorting by number of occurrence
+        Map<String, Integer> sortedDist = distinctMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        sortedDist.forEach((key, value) -> {
+            System.out.println(key + " " + value);
+        });
+        // 20 pairs
+        List<String> keys = new ArrayList<>();
+        Map<String, Integer> dist20Words = new LinkedHashMap<>();
+        sortedDist.keySet().stream().forEach(key -> keys.add(key));
+        for (int i = 0; i < 20; i++) {
+            dist20Words.put(keys.get(i), sortedDist.get(keys.get(i)));
+        }
+        // out sorted 20 pairs
+        dist20Words.forEach((key, value) -> {
+            System.out.println(("Word repeats " +key +" " + value));
+        });
+
+        // names
+
+        List<String> namesUnique = new ArrayList<>();
+
+        Pattern p = Pattern.compile("[A-Z][a-z]{3,}");
+        Matcher m = p.matcher(text);
+        while(m.find()){
+            String nameH = m.group();
+            namesUnique.add(nameH);
+        }
+        // sort in alphabetic
+        Collections.sort(namesUnique);
+        int nameCounter = namesUnique.size();
+
+        System.out.println("Sorted names: "
+                +nameCounter);
+        myWriter.write("--------------------"+System.lineSeparator());
+        myWriter.write(nameCounter);
+        myWriter.write("20 repeated words: "+ String.valueOf(dist20Words)+System.lineSeparator());
+
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(namesUnique.get(i));
+            myWriter.write(namesUnique.get(i)+System.lineSeparator());
+        }
+        myWriter.close();
     }
-
+    // write 20 pairs and names into file
 }
--- src/com/company/Readme.md
+++ src/com/company/Readme.md
@@ -1,19 +1,62 @@
+**TASK**
+1. GLOSSARY - 10 points
+
+    1.1. Download a text about Harry Potter.
+
+    1.2. For each distinct word in the text calculate the number of occurrence.
+
+    1.3. Use RegEx..
+
+    1.4. Sort in the DESC mode by the number of occurrence..
+
+    1.5. Find  the first 20 pairs.
+
+    1.6  Find all the proper names
+
+    1.7.  Count them and arrange in alphabetic order.
+
+    1.8.   First 20 pairs and names write into to a file test.txt
+
+    1.9.  Create a fine header for the file
+
+    1.10  Use Java  Collections to demonstrate your experience (Map, List )
+
+Show all your skills and experience.  All the tricks will be taken into account.
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+**OUTPUT**
  -------HEADER FOR HARRY POTTER TASK--------------
+ 
 First 20 pairs of distinct words: harry occurs 1214 times 
+
 First 20 pairs of distinct words: potter occurs 94 times 
+
 First 20 pairs of distinct words: and occurs 1919 times 
+
 First 20 pairs of distinct words: the occurs 3628 times 
+
 First 20 pairs of distinct words: sorcerer's occurs 16 times 
+
 First 20 pairs of distinct words: stone occurs 75 times 
+
 First 20 pairs of distinct words: chapter occurs 17 times 
+
 First 20 pairs of distinct words: one occurs 255 times 
+
 The word Harry occurs 688 times in the above book
+
 The word Hermiona occurs 688 times in the above book
+
 The word Ron occurs 899 times in the above book
+
 The word Dambldoor occurs 899 times in the above book
+
 The word Voldemort occurs 917 times in the above book
+
 [harry, 1214, potter, 94, and, 1919, the, 3628, sorcerer's, 16, stone, 75, chapter, 17, one, 255]--------------------
+
 ᾤ20 repeated words: {0=7211, to=1855, a=1688, he=1527, of=1259, was=1186, it=1025, in=964, his=937, you=862, =850, said=794, had=702, i=652, on=635, at=625, that=601, they=597, as=526, him=501}
+
 Aaah
 Aargh
 Abbott
